7d813c0aec2249024f5791500e073f80bbab6358
Philip Laxamana <philip.laxamana@hotmail.com>
2018-01-23 19:53:57 -0500

    added express

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => res.send('Hello World!'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
